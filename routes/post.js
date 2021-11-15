dbf2c501a33e1260e2dc3497358b730e41bc6827
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-11-15 20:25:18 +0530

    post done

--- routes/post.js
+++ routes/post.js
@@ -74,4 +74,33 @@ router.put("/:id", async (req, res) => {
     }
   });
 
+  //like / dislike a post
+
+router.put("/:id/like", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post.likes.includes(req.body.userId)) {
+        await post.updateOne({ $push: { likes: req.body.userId } });
+        res.status(200).json("The post has been liked");
+      } else {
+        await post.updateOne({ $pull: { likes: req.body.userId } });
+        res.status(200).json("The post has been disliked");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  //get a post
+
+router.get("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
 module.exports = router;

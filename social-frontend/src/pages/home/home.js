5bd9db7cd8c17555fa0d1b94319ef876636000b9
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-11-19 11:23:15 +0530

    topbar

---
+++ social-frontend/src/pages/home/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
+// import Sidebar from '../../components/sidebar/Sidebar'
+// import Feed from '../../components/feed/Feed'
+
+const home = () => {
+  return (
+    <>
+      <>
+      <Topbar />
+      <div className="homeContainer">
+    
+     
+      </div>
+    </>
+    </>
+  )
+}
+
+export default home
